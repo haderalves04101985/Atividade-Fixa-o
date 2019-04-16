c69075738413db9aa94a7f38915885c73ed2f541
haderalves04101985 <48738148+haderalves04101985@users.noreply.github.com>
2019-04-15 21:48:16 -0300

    Add files via upload

---
+++ src/FuncionarioV1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ifnmg
+ */
+
+public class FuncionarioV1 {
+    String nome;
+    String departamento;
+    Double salario;
+    String data;
+    String rg;
+    boolean ativo;
+};
